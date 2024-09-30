da5995ad90df2b7eaf694aca1a196d999fce366b
Alexis Patiño Oseguera <patinoosegueraalexis@gmail.com>
2024-09-30 10:50:53 -0600

    Se agregaron los cambios del apartado de servicio social en el archivo 'tram_ss.php'

--- info/tram_ss.php
+++ info/tram_ss.php
@@ -132,12 +132,10 @@ noCache();
                    <div class="info-contenido" id="contenido-central">
                     <!-- InstanceBeginEditable name="Contenido" --><br />
                     <p class="titulos-noticas-ext">	<img src="../imgs/SS.jpg" alt="ss" width="100" height="80" longdesc="Preguntas frecuentes" />PREGUNTAS FRECUENTES DE SERVICIO SOCIAL</p>
-                    <br />
-                    <p>¡Importante! Todos los trámites implican tiempo de elaboración y revisión, por lo que no 
-                      son automáticos. La emergencia que estamos enfrentando nos limita en dar una pronta 
-                  respuesta, esperamos  tu comprensión.</p>
-
-
+                    
+                    <p>¡Importante! Todos los trámites implican tiempo de elaboración y revisión, por lo que no son automáticos.</p>
+                    
+                    <p>Todo el proceso del Registro del Servicio Social es de manera digital y debes realizarlo en la siguiente plataforma: <a href="http://132.248.44.97/serviciosocial/">Sistema de Servicio Social</a></p>
 
                   <div id="informacion">
 
@@ -147,97 +145,62 @@ noCache();
 
                         <a href="tram_ss_gustavo.html">PREMIO AL SERVICIO SOCIAL "DR. GUSTAVO BAZ PRADA" 2023</a></p>								
                    </div>
-                   <span>Enlaces para solicitudes</span>
+                   <span>Enlace para solicitudes</span>
                     <div class="Datos">
-                       <p> Sistema de Servicio Social <a href="http://132.248.44.97/serviciosocial">http://132.248.44.97/serviciosocial</a>/</p>                              
+                       <p>Sistema de Servicio Social <a href="http://132.248.44.97/serviciosocial">http://132.248.44.97/serviciosocial</a>/</p>                              
                    </div>
-                   <span>1.¿Puedo  iniciar el servicio social durante este periodo de contingencia sanitaria?</span>
-                   <div class="Datos">
-                    <p>R. Sí, solo si realizas actividades a distancia y lo debe especificar la  institución en la Carta de Aceptación que te entregue. En caso de que las  actividades sean presenciales, será hasta que el semáforo epidemiológico este  en verde y la institución cuente con las condiciones sanitarias necesarias.</p>
-                </div>
-                <span>2.¿Qué  porcentaje de créditos debo tener para comenzar mi servicio social?</span>
+                   
+                <span>1. ¿Qué  requisitos necesito tener para comenzar mi servicio social?</span>
                 <div class="Datos">
-                  <p>R. 70% de créditos del  plan de estudios de tu carrera.</p>
+                  <p>70% de créditos del  plan de estudios de tu carrera.</p>
               </div>
-              <span>3.¿En dónde puedo realizar mi servicio social?</span>
+              <span>2. ¿En dónde puedo realizar mi servicio social?</span>
               <div class="Datos">
-                <p>R.  En cualquier dependencia gubernamental, asociación civil sin fines de lucro  (A.C.), Instituciones de Asistencia&nbsp; Privada (IAP), instituciones  educativa públicas, siempre y cuando tengan programa y clave vigente.</p>
+                <p>En cualquier dependencia gubernamental, asociación civil sin fines de lucro  (A.C.), Instituciones de Asistencia Privada (IAP) o instituciones educativas públicas, siempre y cuando tengan programa y clave vigente.</p>
             </div>
 
-            <span>4.¿En  dónde puedo consultar los programas de servicio social vigentes?</span>
+            <span>3. ¿En  dónde puedo consultar los programas de servicio social vigentes?</span>
             <div class="Datos">
-                <p>R. En la página <a href="https://www.siass.unam.mx">https://www.siass.unam.mx</a>, con tu número de  cuenta.</p>
+                <p>En la página <a href="https://www.siass.unam.mx">https://www.siass.unam.mx</a> con tu número de  cuenta.</p>
             </div>
-            <span>5¿Cuántas  horas debo cubrir del servicio social y en cuánto tiempo?</span>
+            <span>4. ¿Cuántas  horas debo cubrir del servicio social y en cuánto tiempo?</span>
             <div class="Datos">
-              <p>R.  Por reglamento debes cubrir un total de 480 horas en un plazo no menor a seis  meses ni mayor a dos años. </p>
-              <p>&nbsp;</p>
+              <p>Por reglamento, debes cubrir un total de 480 horas en un plazo no menor a seis  meses ni mayor a dos años. </p>
           </div>
-          <span>6.¿Cuándo  y dónde puedo tramitar la carta de presentación que me solicita la institución?</span>
+          <span>5. ¿Cuándo  y dónde puedo tramitar la carta de presentación que me solicita la institución?</span>
           <div class="Datos">
-            <p>R.  Puedes tramitarla, una vez que la Facultad reanude sus actividades  administrativas en la siguiente liga: <a href="http://132.248.44.97/cartapresentacion/vista/index.php">http://132.248.44.97/cartapresentacion/vista/index.php</a></p>
-            <p>&nbsp;</p>
+            <p>El trámite es en línea y puedes realizarlo en la siguiente plataforma: <b><a href="http://132.248.44.97/serviciosocial/">Sistema de Servicio Social</a></b></p>
         </div>
-        <span>7.¿En  cuánto tiempo me entregan la carta de presentación?</span>
+        <span>6. ¿En  cuánto tiempo me entregan la carta de presentación?</span>
         <div class="Datos">
-            <p>R. Entre 7 y 10 días hábiles.</p>
-            <p>&nbsp;</p>
+            <p>Una vez que te aparezca el registro de "aprobado" en la plataforma, podrás descargar la carta de presentación a más tardar al siguiente día hábil de la fecha que realizaste la solicitud.</p>
+
         </div>
-        <span>8.¿Cuántas horas debo cubrir del servicio social y en cuánto tiempo?</span>
+        
+        <span>7. Una vez iniciado el servicio social ¿qué trámite tengo que realizar?</span>
         <div class="Datos">
-            <p>IR. Por reglamento debes cubrir un total de 480 horas en un plazo no  menor a seis meses ni mayor a dos años.</p>
-            <p>&nbsp;</p>
-        </div>
-        <span>9.                                
-        Si  inicié mi servicio social durante los meses de febrero y marzo del año en curso  y por motivos de la contingencia sanitaria no pude entregar la carta de  aceptación ¿Qué debo hacer? </span>
-        <div class="Datos">
-            <p>R.  Por favor envíala al correo <a href="mailto:serviciosocial.tramites@aragon.unam.mx">serviciosocial@aragon.unam.mx</a> en archivo PDF, así como tu historia académica que puedes obtener en https://www.dgae-siae.unam.mx/www_gate.php. En caso de que proceda el  registro, se te enviará la Carta de Registro por el mismo medio.</p>
-            <p>&nbsp;</p>
-        </div>
-        <span>10.Si inicié mi servicio social durante el período de contingencia, pero  realizando actividades a distancia ¿Qué trámite tengo que realizar para  registrar el servicio? </span>
-        <div class="Datos">
-            <p>R.  Tienes que pedir al responsable de servicio social de la institución que envíe  a través de su correo institucional, la carta de aceptación (en hoja  membretada, firmada por el responsable en siass y con el sello de la  institución) al correo <a href="mailto:serviciosocial.tramites@aragon.unam.mx">serviciosocial@aragon.unam.mx</a>. </p>
-            <p>&nbsp;</p>
-        </div>
-        <span>11.¿ConcluÍ  el servicio social con fecha anterior a que se declarará la contingencia  sanitaria y cuento la carta de término emitida por la institución y el informe  global firmado y sellado por el área donde lo realice ¿Cómo y dónde puedo  entregar mis documentos? </span>
-        <div class="Datos">
-            <p>R.  Tienes que enviar ambos documentos en archivo PDF al correo <a href="mailto:serviciosocial@aragon.unam.mx">serviciosocial@aragon.unam.mx</a> para proceder a su revisión y en su  caso trámite de liberación. </p>
-            <p>&nbsp; </p>
+            <p>La institución te debe entregar la carta de aceptación en la página del <b><a href="http://132.248.44.97/serviciosocial/">Sistema de Servicio Social</a></b>, misma que deberás escanear y subir en un archivo PDF en el apartado con este nombre.</p>
         </div>
 
-        <span>12.Si  concluÍ el servicio social durante el período de contingencia y la institución  me envíó por correo la carta de término, así como el informe global firmado y  sellado por el área donde realicÉ el servicio ¿Qué debo hacer? </span>
+        <span>8. ¿ConcluÍ  el servicio social y ya cuento con la carta de término emitida por la institución, junto con el informe global sellado por el área donde lo realicé ¿Cómo y dónde puedo  entregar mis documentos? </span>
         <div class="Datos">
-            <p>R.  Tienes que pedir al responsable de servicio social de la institución que envíe  a través de su correo institucional, la carta de término (en hoja membretada,  firmada por el responsable en siass y con el sello de la institución) y el  informe global al correo <a href="mailto:serviciosocial@aragon.unam.mx">serviciosocial@aragon.unam.mx</a>. </p>
-            <p>&nbsp;</p>
+            <p>R.  Tienes que subir ambos documentos en archivo PDF en la plataforma del <b><a href="http://132.248.44.97/serviciosocial/">Sistema de Servicio Social</a></b> en el apartado correspondiente para proceder a su revisión y proseguir con el trámite de liberación</p>
         </div>
-        <span>13.¿Cómo debo elaborar el informe global de actividades?</span>
+
+        <span>9. ¿Cómo debo elaborar el informe global de actividades?</span>
         <div class="Datos">
             <p><br />
-            R.  Es un formato libre, &nbsp;debe estar dirigido a la Lic. Guadalupe Mendieta  Bello, Jefa del Departamento de Servicio Social, anotando tus datos y los del  programa donde participaste, además describir las actividades que realizaste  durante tu servicio social, el documento debe estar firmado por el prestador.</p>
-            <p>&nbsp;</p>
+            R.  Es un formato libre, debe estar dirigido a la Lic. Guadalupe Mendieta Bello, Jefa del Departamento de Servicio Social, anotando tus datos y los del programa donde participaste, además describir las actividades que realizaste durante tu servicio social, el documento debe estar firmado por el prestador del servicio y por la persona que supervisó las actividades, así como el sello de la institución o del área donde prestaste tu servicio.</p>
+     
         </div>
 
-        <span>14.EntreguÉ  mi carta de término e informe global en Servicio Social antes de la  contingencia y solo me faltó recoger mi liberación ¿Cómo puedo obtener éste  documento?</span>
+        <span>10. Para  darme de baja del programa de servicio social en el que me registré ¿Qué debo  hacer?</span>
         <div class="Datos">
-            <p><br />R.  Únicamente si te encuentras en proceso de titulación o la requieras para  inscribirte a un diplomado con opción de titulación, deberás enviar al correo <a href="mailto:serviciosocial@aragon.unam.mx">serviciosocial@aragon.unam.mx</a> tú número de cuenta  y adjuntar el acuse de la carta de término con el sello de Tramitado de  Servicio Social, señalando el motivo por el que requieres la liberación. En  caso de que ya se cuente con éste documento, se enviará a tu correo en archivo  PDF con la firma digitalizada del Director General de Orientación y Atención  Educativa de la UNAM.  <br />
-            La  liberación y carta de término original, se te entregará el día en que previa  cita programada por el Departamento de Servicios Escolares tengas que acudir a  la Facultad. </p>
-            <p>&nbsp;</p>
-        </div>
-        <span>15.									 
-        Para  darme de baja del programa de servicio social en que me registré ¿Qué debo  hacer?</span>
-        <div class="Datos">
-
-
-         <p> R.  Enviar al correo <a href="mailto:serviciosocial@aragon.unam.mx">serviciosocial@aragon.unam.mx</a> un escrito dirigido  a la Lic. Guadalupe Mendieta Bello, Jefa del Departamento de Servicio Social de  la FES Aragón, donde señales la fecha, tu nombre, número de cuenta y carrera,  así como nombre y clave del programa, además de anotar una breve exposición del  motivo por el cual solicitas la baja del servicio social, y deberá estar  avalada con tu firma. El documento debes enviar en archivo PDF.</p>
+         <p>Enviar al correo <a href="mailto:serviciosocial@aragon.unam.mx">serviciosocial@aragon.unam.mx</a> o presentar de manera física en el Departamento de Servicio Social, un escrito dirigido a la Lic. Guadalupe Mendieta Bello, Jefa del Departamento de Servicio Social de la FES Aragón, donde señales la fecha, tu nombre, número de cuenta y carrera, así como nombre y clave del programa, además de anotar una breve exposición del motivo por el cual solicitas la baja del servicio social, y deberá estar avalada con tu firma. Debes de enviar el documento en un archivo PDF.</p>
      </div>
-     <span>¿Alguna otra pregunta?</span>
-     <div class="Datos">
 
-
-         <p>Escribe	a	serviciosocial@aragon.unam.mx con tu duda, nombre y número de cuenta. </p>
-     </div>
      <p>Es responsabilidad del alumno proporcionar la información completa y verídica que se 
-     solicita para cada trámite para que se expidan lo más pronto posible.</p>
+     solicita para cada trámite y de esta forma se expidan lo más pronto posible.</p>
  </div>
  <!-- InstanceEndEditable -->                            </div>
  <!--termina contenido central-->
